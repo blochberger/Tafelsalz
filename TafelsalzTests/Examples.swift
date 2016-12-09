7347dfe032d79dab6469b996cfa150782f220eeb
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2016-12-09 15:19:20 +0100

    Remove Xcode generated license header

--- TafelsalzTests/Examples.swift
+++ TafelsalzTests/Examples.swift
@@ -1,11 +1,3 @@
-//
-//  Examples.swift
-//  Tafelsalz
-//
-//  Created by Maximilian Blochberger on 2016-10-31.
-//  Copyright © 2016 Universität Hamburg. All rights reserved.
-//
-
 import XCTest
 @testable import Tafelsalz
 
